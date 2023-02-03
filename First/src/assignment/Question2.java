5e925431ce0f92df2f8799bee8ca79d6feefd11a
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-03 17:43:28 +0530

    done question 2

---
+++ First/src/assignment/Question2.java
@@ -0,0 +1,25 @@
+/*
+Author: Animesh Kumar
+Date: 03/02/2023 (DD/MM/YYYY)
+Purpose: Learning Java syntax and some conditional statements
+Status: Done the second question.
+Question: Take name as input and print a greeting message for that particular name.
+*/
+
+package assignment;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Namaste " + name + "!");
+
+    }
+}
