4ec17d79ad82fbf01c2e356834120a04b88bf38d
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-03 17:59:18 +0530

    question 3 done

---
+++ First/src/assignment/Question3.java
@@ -0,0 +1,30 @@
+/*
+Author: Animesh Kumar
+Date: 03/02/2023 (DD/MM/YYYY)
+Purpose: Learning Java syntax and some conditional statements
+Status: Done the second question.
+Question: Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+*/
+
+package assignment;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter principle: ");
+        float principle = input.nextFloat();
+
+        System.out.print("Enter rate: ");
+        float rate = input.nextFloat();
+
+        System.out.print("Enter time (in years): ");
+        float time = input.nextFloat();
+
+        float si = (principle*rate*time)/100;
+
+        System.out.println("The simple interest calculated is: " + si);
+    }
+}
