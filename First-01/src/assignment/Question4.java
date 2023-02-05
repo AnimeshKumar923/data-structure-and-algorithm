d5b391d0f3dc36f0a92804237819bd7f9bdeb3be
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-05 14:12:37 +0530

    doubt resolved

--- First-01/src/assignment/Question4.java
+++ First-01/src/assignment/Question4.java
@@ -2,7 +2,7 @@
 Author: Animesh Kumar
 Date: 03/02/2023 (DD/MM/YYYY)
 Purpose: Learning Java syntax and some conditional statements
-Status: Doubt in the question.
+Status: Completed successfully
 Question: Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
 */
 
@@ -23,19 +23,17 @@ public class Question4 {
         float ans = 0;
 
         System.out.print("Enter the operation you want to perform: ");
-        char operand = input.next().charAt(0); // what is this? I just copied it from GFG website...
+        char operand = input.next().charAt(0); // understood it. It takes the character at the index position given.
+                                               // Here it is the 0th index character.
 
-        if (operand == '+'){
+        if (operand == '+') {
             ans = num1 + num2;
-        }
-        else if (operand == '-') {
-            ans=num1-num2;
-        }
-        else if (operand == '*') {
-            ans=num1*num2;
-        }
-        else if (operand == '/') {
-            ans=num1/num2;
+        } else if (operand == '-') {
+            ans = num1 - num2;
+        } else if (operand == '*') {
+            ans = num1 * num2;
+        } else if (operand == '/') {
+            ans = num1 / num2;
         }
 
         System.out.print("Result is: " + ans);
