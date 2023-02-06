be3371e58782688db662e670887389459494ab2b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-06 15:51:41 +0530

    issue resolved with question 2

--- conditional-loops-02/src/assignment02/basic/Question02.java
+++ conditional-loops-02/src/assignment02/basic/Question02.java
@@ -2,7 +2,7 @@
 Author: Animesh Kumar
 Date: 06/02/2023 (DD/MM/YYYY)
 Purpose: Learning conditionals and loops
-Status: Stuck. Answer is always zero.
+Status: Completed successfully
 Question: Area Of Triangle
 */
 
@@ -14,15 +14,15 @@ public class Question02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double area;
+
         System.out.print("Enter base of triangle: ");
         double b = input.nextFloat();
 
         System.out.print("Enter height of triangle: ");
         double h = input.nextFloat();
 
-        double area = 0;
-
-        area = (b * h * (1 / 2));
+        area = (b * h) / 2;
 
         System.out.println("Area: " + area);
     }
