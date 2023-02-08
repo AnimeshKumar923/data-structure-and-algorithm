ceafe81cceb4c30ae6662f8ae2ea89dd7620776b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-08 23:33:28 +0530

    completed question 23, struggled a bit

--- conditional-loops-02/src/assignment02/basic/Question23.java
+++ conditional-loops-02/src/assignment02/basic/Question23.java
@@ -2,32 +2,35 @@
 Author: Animesh Kumar
 Date: 08/02/2023 (DD/MM/YYYY)
 Purpose: Learning conditionals and loops
-Status: In progress
+Status: Completed successfully
 Question: Input a number and print all the factors of that number (use loops).
 */
+// struggle with the algorithm part, how to build the factor logic but did it at last...:)
 
 package assignment02.basic;
 
 import java.util.Scanner;
 
-public class Question22 {
+public class Question23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int n=input.nextInt();
+        int n = input.nextInt();
 
-        while(n>0){
-            int count=0;
-            int divisor=1;
+        int count = 0;
+        int divisor = 1;
 
-            int factor=n%divisor;
-            
-            if(factor==0){
-                System.out.print(divisor+" ");
+        while (divisor <= n) {
+
+            int factor = n % divisor;
+
+            if (factor == 0) {
+                System.out.print(divisor + " ");
                 count++;
                 divisor++;
-                n=n/divisor;
+            } else {
+                divisor++;
             }
         }
     }
