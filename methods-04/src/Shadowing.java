ae71968b385af45aa4fa6cd886df064dfd1b3f4b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-26 12:22:02 +0530

    learn about shadowing

---
+++ methods-04/src/Shadowing.java
@@ -0,0 +1,20 @@
+public class Shadowing {
+    static int x = 90; // this will be shadowed at line 12
+
+    public static void main(String[] args) {
+        System.out.println(x); // 90
+        int x; // the class variable at line 2 is shadowed by this
+
+        // System.out.println(x); // scope wil being when value is initialized
+
+        x = 40;
+
+        System.out.println(x); // 40
+
+        fun();
+    }
+
+    static void fun() {
+        System.out.println(x);
+    }
+}
