6944f4a37e8f75f1dd387fbba233b3af694a2be7
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-26 12:32:09 +0530

    learn about variable length arguments

---
+++ methods-04/src/VarArgs.java
@@ -0,0 +1,19 @@
+// variable length arguments
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(1, 5, 448, 58, 61, 56);
+
+        multiple(2, 74, "array", "String", "variable");
+    }
+
+    static void multiple(int a, int b, String... u) { // variable length arguments always at the end
+        System.out.println(Arrays.toString(u));
+    }
+
+    static void fun(int... v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
