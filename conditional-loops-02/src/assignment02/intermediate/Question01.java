507862f929bf2156ce94b4818359576d96f8b9bb
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-28 15:56:17 +0530

    start intermediate assignment

---
+++ conditional-loops-02/src/assignment02/intermediate/Question01.java
@@ -0,0 +1,12 @@
+// Factorial program in Java
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
