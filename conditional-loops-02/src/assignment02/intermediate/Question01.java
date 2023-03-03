e1eaeeb068a7e6f8088bbdbca102b43a089b3d05
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-04 00:17:25 +0530

    completed question01 of intermediate, factorial

--- conditional-loops-02/src/assignment02/intermediate/Question01.java
+++ conditional-loops-02/src/assignment02/intermediate/Question01.java
@@ -1,4 +1,10 @@
-// Factorial program in Java
+/*
+Author: Animesh Kumar
+Date: 03/03/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: Completed successfully
+Question: Factorial program in Java
+*/
 
 package assignment02.intermediate;
 
@@ -8,5 +14,21 @@ public class Question01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("Enter number to find factorial: ");
+        int num = in.nextInt();
+        int res = fact(num);
+        System.out.println(res);
+    }
+
+    static int fact(int num) {
+        int f = 1;
+        if (num == 0) {
+            System.out.println(1);
+        } else {
+            for (int i = 1; i <= num; i++) {
+                f = f * i;
+            }
+        }
+        return f;
     }
 }
