88c1624f9c718d722d6c95baa0d42315c9a23836
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-15 22:54:54 +0530

    Complete question-04, modify status of question-03

---
+++ conditional-loops-02/src/assignment02/intermediate/Question04.java
@@ -0,0 +1,27 @@
+/*
+Author: Animesh Kumar
+Date: 15/03/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: In progress...
+Question: Calculate Discount Of Product
+*/
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        float price, discount;
+        System.out.print("Enter product price: ");
+        price = in.nextFloat();
+
+        System.out.print("Enter discount in percentage: ");
+        discount = in.nextFloat();
+
+        float discount_price = price - ((discount / 100) * price);
+        System.out.println("Discounted price= " + discount_price);
+    }
+}
