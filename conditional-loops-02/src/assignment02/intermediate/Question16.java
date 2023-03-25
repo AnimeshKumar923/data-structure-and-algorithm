2d21188f9c837048d77938e7c2c20b0ab86b6f8b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-25 16:22:37 +0530

    Question related to strings in JAVA skipped

---
+++ conditional-loops-02/src/assignment02/intermediate/Question16.java
@@ -0,0 +1,17 @@
+/*
+Author: Animesh Kumar
+Date: 24/03/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: Will complete after doing strings in JAVA...
+Question: Reverse A String In Java
+*/
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
---
+++ conditional-loops-02/src/assignment02/intermediate/Question17.java
@@ -0,0 +1,17 @@
+/*
+Author: Animesh Kumar
+Date: 24/03/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: Will complete after doing strings in JAVA...
+Question: Find if a number is palindrome or not
+*/
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
