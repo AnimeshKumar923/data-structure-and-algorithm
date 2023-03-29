1ea3b50c9dfb4c8b5cd29eb8f1044ab487fb853c
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-29 11:29:23 +0530

    Question-21 In progress

---
+++ conditional-loops-02/src/assignment02/intermediate/Question21.java
@@ -0,0 +1,20 @@
+/*
+Author: Animesh Kumar
+Date: 29/03/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: Completed successfully
+Question: Java Program Vowel Or Consonant
+*/
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char letter = in.next().charAt(0);
+
+    }
+}
