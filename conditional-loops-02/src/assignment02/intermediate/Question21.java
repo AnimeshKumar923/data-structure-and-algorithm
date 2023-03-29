d43a17d74807db76579dbbdc660c848d76f095a9
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-29 21:48:47 +0530

    Complete Question-21

--- conditional-loops-02/src/assignment02/intermediate/Question21.java
+++ conditional-loops-02/src/assignment02/intermediate/Question21.java
@@ -14,7 +14,18 @@ public class Question21 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter a letter: ");
         char letter = in.next().charAt(0);
+        boolean caseSense;
+
+        caseSense = (letter == 'A' || letter == 'a' || letter == 'E' || letter == 'e' || letter == 'I' || letter == 'i'
+                || letter == 'O' || letter == 'o' || letter == 'U' || letter == 'u');
+
+        if (caseSense) {
+            System.out.println(letter + " is vowel");
+        } else {
+            System.out.println(letter + " is consonant");
+        }
 
     }
 }
