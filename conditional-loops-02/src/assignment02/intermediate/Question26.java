cc9da91e0e7bd29d68e558da689fe72071c61c33
Animesh Kumar <kumaranimesh923@gmail.com>
2023-04-04 11:59:59 +0530

    Question-26 in progress
    Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.

---
+++ conditional-loops-02/src/assignment02/intermediate/Question26.java
@@ -0,0 +1,25 @@
+/*
+Author: Animesh Kumar
+Date: 04/04/2023 (DD/MM/YYYY)
+Purpose: Learning conditionals and loops
+Status: Completed successfully
+Question: Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.
+
+*/
+
+package assignment02.intermediate;
+
+import java.util.Scanner;
+
+public class Question24 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num=in.nextInt();
+        
+        while(true){
+            System.out.print("Enter 1 for negative addition\n2 for sum of positive even numbers\n3 for sum of positive odd numbers")
+        }
+
+    }
+}
