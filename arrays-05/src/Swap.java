1753618ddb8c4ad0ea39fdcc4f545079a666f1fb
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-06-17 21:22:42 +0530

    Program to Swap two numbers in an array
    This program takes array and indices from the user and passes into a
    swap function, then does the calculation and produces the desired output

---
+++ arrays-05/src/Swap.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter size of 1-D array: ");
+        int size = in.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.print("Enter elements in array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("Elements before swap: " + Arrays.toString(arr));
+
+        System.out.println("Enter elements position to interchange: ");
+        int element1 = in.nextInt();
+        int element2 = in.nextInt();
+        swap(arr, element1 - 1, element2 - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
