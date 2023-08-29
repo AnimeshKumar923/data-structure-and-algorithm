5795260bba40841c64c82bebd0ded2efb177cdb7
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-08-30 00:09:35 +0530

    start linked list concept + code

---
+++ 11-linked-list/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(9);
+        list.insertFirst(17);
+
+        list.display();
+    }
+}
