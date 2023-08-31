308a9c89443fc11bea982b1a7aeacc25b4279645
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-09-01 00:05:55 +0530

    Rename linked list file, add more methods
    Changes:
    
    - Add method to insert element at first postion.
    - Add method to insert element at last position.
    - Add method to insert element at any given position.
    - Add method to display elements.

---
+++ 11-linked-list/src/LL.java
@@ -0,0 +1,87 @@
+public class LL {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public LL(){
+        this.size=0;
+    }
+
+    public void insertFirst(int value){
+        Node node=new Node(value);
+        node.next=head;
+        head=node;
+
+        if(tail==null){
+            tail=head;
+        }
+
+        size+=1;
+    }
+
+    public void insertLast(int value){
+
+        if(tail==null){
+            insertFirst(value);
+            return;
+        }
+
+        Node node=new Node(value);
+        tail.next=node;
+        tail=node;
+        size++;
+
+    }
+
+    public void insert(int value, int index){
+        if(index==0){
+            insertFirst(value);
+            return;
+        }
+
+        if(index==size){
+            insertLast(value);
+            return;
+        }
+
+        Node temp=head; // i starts from one because this is already at 0th index.
+
+        for(int i=1; i<index; i++){
+            temp=temp.next;
+        }
+
+        Node node=new Node(value, temp.next);
+        temp.next=node;
+        
+        size++;
+    }
+
+    public void display(){
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.print(temp.value+" --> ");
+            temp=temp.next;
+        }
+        System.out.println("END");
+        
+        System.out.println("Size = "+size);
+    }
+
+    
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value=value;
+        }
+
+        public Node(int value, Node next){
+            this.value=value;
+            this.next=next;
+        }
+    }
+}
--- 11-linked-list/src/Main.java
+++ 11-linked-list/src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList list=new LinkedList();
+        LL list=new LL();
 
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(9);
         list.insertFirst(17);
-
+        list.insertLast(45);
+        list.insert(125, 5);
+        
         list.display();
+
     }
 }
