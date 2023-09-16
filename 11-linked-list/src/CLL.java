3ec7724164f81eece309a6c4ec681468ca5abdb3
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-09-17 00:51:43 +0530

    circular linked list - insert, display method

---
+++ 11-linked-list/src/CLL.java
@@ -0,0 +1,46 @@
+public class CLL {
+
+    private Node head;
+    private Node tail;
+
+    public CLL(){
+        this.head=null;
+        this.tail=null;
+    }
+
+    public void insert(int value){
+        Node node=new Node(value);
+
+        if(head==null){
+            head=node;
+            tail=node;
+            return;
+        }
+
+        tail.next=node;
+        node.next=head;
+        tail=node;
+    }
+
+    public void display(){
+        Node node=head;
+
+        if(head!=null){
+            do {
+                System.out.print(node.value+" -> ");
+                node=node.next;
+            } while (node!=head);
+        }
+
+        System.out.println("HEAD");
+    }
+
+    private class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value=value;
+        }
+    }
+}
--- 11-linked-list/src/Main.java
+++ 11-linked-list/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // singly linked-list
+        
         // LL list=new LL();
 
         // list.insertFirst(3);
@@ -20,17 +22,36 @@ public class Main {
         // list.delete(1);
         // list.display();
 
-        DLL listdll=new DLL();
 
-        listdll.insertFirst(87);
-        listdll.insertFirst(65);
-        listdll.insertFirst(7);
-        listdll.insertFirst(20);
 
-        listdll.insertLastWithoutTail(587);
 
-        listdll.insert(65, 254);
+        // doubly linked-list
+        // DLL listdll=new DLL();
+        
+        // listdll.insertFirst(87);
+        // listdll.insertFirst(65);
+        // listdll.insertFirst(7);
+        // listdll.insertFirst(20);
 
-        listdll.display();
+        // listdll.insertLastWithoutTail(587);
+
+        // listdll.insert(65, 254);
+
+        // listdll.display();
+
+
+
+
+
+        // circular linked-list
+        CLL listcll=new CLL();
+
+        listcll.insert(54);
+        listcll.insert(66);
+        listcll.insert(87);
+        listcll.insert(20);
+        listcll.insert(8);
+
+        listcll.display();
     }
 }
